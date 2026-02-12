071cf630a8cd566df57ec616b2a36633d26153cf
sonnnai-hayashi <sonnnai-hayashi@dac.co.jp>
2026-02-12 17:48:51 +0900

    ryuichi-hoshino-dtvn

--- membership_r.tf
+++ membership_r.tf
@@ -38,7 +38,7 @@ resource "github_membership" "tsubota-ryo" {
   username = "tsubota-ryo" #  ryo.tsubota@hakuhodody-media.co.jp
 }
 
-resource "github_membership" "ryuichi-hoshino" {
+resource "github_membership" "ryuichi-hoshino-dtvn" {
   username = "ryuichi-hoshino" # ryuichi.hoshino@hakuhodody-one.co.jp / 技術統括本部 情報システム局 業務システム開発部 / HOUSEI
 }
 
